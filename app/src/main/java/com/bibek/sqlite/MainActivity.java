26224e98fec139dd922edc12b3097768e55c5b11
Bvekv1 <bvekp1@gmail.com>
2019-12-05 12:57:12 +0545

    database

--- app/src/main/java/com/bibek/sqlite/MainActivity.java
+++ app/src/main/java/com/bibek/sqlite/MainActivity.java
@@ -2,9 +2,13 @@ package com.bibek.sqlite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.bibek.sqlite.myhelper.MyHelper;
 
 public class MainActivity extends AppCompatActivity {
     EditText etWord, etMeaning;
@@ -18,6 +22,17 @@ public class MainActivity extends AppCompatActivity {
         etWord = findViewById(R.id.etWord);
         etMeaning = findViewById(R.id.etMeaning);
         btnSearch = findViewById(R.id.btnSearch);
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyHelper myHelper = new MyHelper(MainActivity.this);
+
+                SQLiteDatabase sqLiteDatabase = myHelper.getWritableDatabase();
+
+
+            }
+        });
 
     }
 }
